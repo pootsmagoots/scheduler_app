7a7214b57f0e92bcc0be5f6f52e961be7daca9e0
Travis Batts <Travisbatts6@gmail.com>
2017-02-27 17:30:16 -0500

    with meeting

--- scheduler_app/db/schema.rb
+++ scheduler_app/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170227183527) do
+ActiveRecord::Schema.define(version: 20170227222320) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,13 @@ ActiveRecord::Schema.define(version: 20170227183527) do
     t.string  "img_url"
     t.integer "month_id"
     t.index ["month_id"], name: "index_events_on_month_id", using: :btree
+  end
+
+  create_table "meetings", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "start_time"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "months", force: :cascade do |t|
